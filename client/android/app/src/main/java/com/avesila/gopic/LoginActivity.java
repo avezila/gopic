21579e86964c900a7853be7ea8c98338409e2e3b
VisborN <dudko.vlad@gmail.com>
2016-07-08 01:52:20 +0300

    added changing address in registration

--- client/android/app/src/main/java/com/avesila/gopic/LoginActivity.java
+++ client/android/app/src/main/java/com/avesila/gopic/LoginActivity.java
@@ -17,9 +17,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -30,18 +35,24 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import auth.AuthGrpc;
 import auth.AuthOuterClass;
+import auth.AuthOuterClass.LoginReq;
+import auth.AuthOuterClass.LoginRes;
+import auth.AuthOuterClass.RegisterReq;
+import auth.AuthOuterClass.RegisterRes;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  * //todo verify for errors etc.
- * //todo may be split to different classes
+ * //todo SPLIT to different classes!!!!
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
@@ -58,6 +69,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     // UI references.
     private EditText mLoginView;
     private EditText mPasswordView;
+    private EditText mAddressView;
     private AutoCompleteTextView mEmailView;
     private View mProgressView;
     private View mLoginFormView;
@@ -67,12 +79,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        mAddressView = (EditText) findViewById ( R.id.address );
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
         mLoginView = (EditText) findViewById ( R.id.login );
 
         mPasswordView = (EditText) findViewById(R.id.password);
+        
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -83,6 +98,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
         });
+        
+        mEmailView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
 
         mSignInButton = (Button) findViewById( R.id.sign_in_button);
         mSignInButton.setOnClickListener(new OnClickListener() {
@@ -98,30 +125,42 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             public void onClick(View view) {
                 if ( mEmailView.getVisibility () == View.VISIBLE ) {
                     attemptLogin ();
-                } else {//todo make material animation
+                } else {//todo may be change this animation
                     
-                    int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+                    mPasswordView.setImeActionLabel (
+                            getString ( R.string.action_register )
+                            , R.id.login );
+                    
+                    final Animation animEmailView = AnimationUtils.makeInAnimation (
+                            LoginActivity.this, true );
+                    animEmailView.setDuration ( 500 );
+                    animEmailView.setInterpolator( new DecelerateInterpolator () );
+                
+                    Animation animSignInButton = AnimationUtils.makeOutAnimation ( 
+                            LoginActivity.this, true );
+                    animSignInButton.setDuration ( 500 );
+                    animSignInButton.setInterpolator( new AccelerateInterpolator () );
+                    animSignInButton.setAnimationListener ( new Animation.AnimationListener () {
 
-                    mSignInButton.setVisibility(View.GONE);
-                    mSignInButton.animate()
-                                 .setDuration(shortAnimTime)
-                                 .alpha( 0 ).setListener(new AnimatorListenerAdapter() {
                         @Override
-                        public void onAnimationEnd(Animator animation) {
-                            mSignInButton.setVisibility(View.GONE);
+                        public void onAnimationStart ( Animation animation ) {
+                            //nothing to do
                         }
-                    });
 
-                    mEmailView.setVisibility(View.VISIBLE);
-                    mEmailView.animate()
-                              .setDuration(shortAnimTime)
-                              .alpha(1)
-                              .setListener(new AnimatorListenerAdapter() {
                         @Override
-                        public void onAnimationEnd(Animator animation) {
-                            mEmailView.setVisibility(View.VISIBLE);
+                        public void onAnimationEnd ( Animation animation ) {
+                            mSignInButton.setVisibility ( View.GONE );
+                            mEmailView.setVisibility ( View.VISIBLE );
+                            mEmailView.startAnimation ( animEmailView );
                         }
-                    });
+
+                        @Override
+                        public void onAnimationRepeat ( Animation animation ) {
+                            //it would not repeat
+                        }
+                    } );
+                    mSignInButton.startAnimation ( animSignInButton );
+                    
                 }
             }
         });
@@ -189,22 +228,27 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
+        String address = mAddressView.getText ().toString ();
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
-        String loginName = mLoginView.getText ().toString ();
+        String username = mLoginView.getText ().toString ();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+        if ( TextUtils.isEmpty ( password ) ) {
+            mPasswordView.setError ( getString ( R.string.error_field_required ) );
+            focusView = mPasswordView;
+            cancel = true;
+        } else if ( !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
         }
 
         // Check for a valid email address.
-        if (mEmailView.isShown ()) {
+        if ( mEmailView.getVisibility () == View.VISIBLE ) {
             if ( TextUtils.isEmpty ( email ) ) {
                 mEmailView.setError ( getString ( R.string.error_field_required ) );
                 focusView = mEmailView;
@@ -216,6 +260,29 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         }
 
+        if ( !address.contains ( ":" ) ){
+            address = address + ":" + getString ( R.string.standard_port );
+        }
+        if ( TextUtils.isEmpty ( address ) ) {
+            mAddressView.setError ( getString ( R.string.error_field_required ) );
+            focusView = mAddressView;
+            cancel = true;
+        } else if ( !isAddressValid ( address ) ) {
+            mAddressView.setError ( getString ( R.string.error_incorrect_address ) );
+            focusView = mAddressView;
+            cancel = true;
+        }
+        
+        if ( TextUtils.isEmpty ( username ) ) {
+            mLoginView.setError ( getString ( R.string.error_field_required ) );
+            focusView = mLoginView;
+            cancel = true;
+        } else if ( !isUsernameValid ( username ) ) {
+            mLoginView.setError ( getString ( R.string.error_invalid_login ) );
+            focusView = mLoginView;
+            cancel = true;
+        }
+
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
@@ -224,22 +291,44 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mLoginTask = new LoginTask (loginName, password);
+            mLoginTask = new LoginTask (address, username, password);
             if (mEmailView.getVisibility () == View.VISIBLE) {
                 mLoginTask.register ( email );
             }
             mLoginTask.execute();
         }
     }
+    private boolean isUsernameValid(String username) {
+        //TODO: Replace this with your own logic
+        return true;
+    }
+    
+    private boolean isAddressValid( String address){
+        boolean valid = true;
+        String [] addressArr = TextUtils.split ( address, ":" );
+        if (addressArr.length != 2) {
+            valid = false;
+        }else if (!addressArr[0].matches(
+                "^([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}$" )//domain 
+                  // todo make local symbols 
+            && !addressArr[0].matches ( 
+                "((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)" ) //ipv4
+            && !addressArr[0].matches ( "((^|:)([0-9a-fA-F]{0,4})){1,8}$" )) {//ipv6
+            valid = false;
+        }
+        return valid;
+    }
+    
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.matches ( "^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$" );
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         return password.length() > 4;
     }
+    
 
     /**
      * Shows the progress UI and hides the login form.
@@ -331,13 +420,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         private String                    loginName, password, email, session;
         private List<AuthOuterClass.Error> errorList;
 
-        public LoginTask ( String loginName, String password ) {
+        public LoginTask ( String address, String loginName, String password ) {
+            String [] addressArr = address.split ( ":" );
             channel = ManagedChannelBuilder
-                    .forAddress ( "gopic.ru", 5353 )
+                    .forAddress ( addressArr [ 0 ], Integer.valueOf ( addressArr [ 1 ] ) )
                     .usePlaintext ( true )
                     .build ();
-
+            
+            
             blockingStub = AuthGrpc.newBlockingStub ( channel );
+            
 
             this.loginName = loginName;
             this.password = password;
@@ -351,34 +443,49 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground ( Void... voids ) {
+            try {
+                
+                if ( email != null ) {
+                    RegisterReq registerReq = AuthOuterClass.RegisterReq
+                            .newBuilder ()
+                            .setLogin ( loginName )
+                            .setPassword ( password )
+                            .setEmail ( email )
+                            .build ();
 
-            if ( email != null ) {
-                AuthOuterClass.RegisterReq registerReq = AuthOuterClass.RegisterReq
-                        .newBuilder ()
-                        .setLogin ( loginName )
-                        .setPassword ( password )
-                        .setEmail ( email )
-                        .build ();
+                    RegisterRes registerRes = blockingStub
+                            .withDeadlineAfter ( 5, TimeUnit.SECONDS )
+                            .register ( registerReq );
 
-                AuthOuterClass.RegisterRes registerRes = blockingStub.register ( registerReq );
+                    errorList = registerRes.getErrorList ();
+                    session = registerRes.getSession ();
 
-                errorList = registerRes.getErrorList ();
-                session = registerRes.getSession ();
+                } else {
+                    
+                    LoginReq loginReq = AuthOuterClass.LoginReq
+                            .newBuilder ()
+                            .setLogin ( loginName )
+                            .setPassword ( password )
+                            .build ();
 
-            }else{
-                AuthOuterClass.LoginReq loginReq = AuthOuterClass.LoginReq
-                        .newBuilder ()
-                        .setLogin ( loginName )
-                        .setPassword ( password )
-                        .build ();
-
-                AuthOuterClass.LoginRes  loginRes = blockingStub.login ( loginReq );
-
-                errorList = loginRes.getErrorList ();
-                session = loginRes.getSession ();
-
+                    LoginRes loginRes = blockingStub
+                            .withDeadlineAfter ( 5, TimeUnit.SECONDS  )
+                            .login ( loginReq );
+                    Log.w( "loginRes initialized", loginRes.isInitialized () + "" );
+                    errorList = loginRes.getErrorList ();
+                    session = loginRes.getSession ();
+                }
+            } catch ( StatusRuntimeException e ) {
+                
+                
             }
-
+            
+            catch ( Exception e ){
+                e.printStackTrace ( System.err );
+                errorList = new ArrayList<> ();
+                errorList.add ( AuthOuterClass.Error.INTERNAL );
+            }
+                
             return errorList.isEmpty ();
         }
 
@@ -423,7 +530,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                         case INTERNAL:
                         default:
                             Toast.makeText ( LoginActivity.this
-                                    , "Something went wrong"
+                                    , getString ( R.string.something_went_wrong )
                                     , Toast.LENGTH_SHORT )
                                  .show ();
                     }
